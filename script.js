c43d43b904a06a5e3dde3c47d9b38417476c2ade
Anass Lamouri <anasslamouri@icloud.com>
2025-08-06 15:15:47 +0100

    Add if statement to buttonValue

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const buttonsEl = document.querySelectorAll("button")
 
 
+
 for (let i = 0; i < buttonsEl.length; i++) {
   buttonsEl[i].addEventListener("click", () => {
-    console.log(buttonsEl[i].textContent)
+    const buttonValue = buttonsEl[i].textContent
+    if (buttonValue === "C") {
+      clearResult()
+    } else if (buttonValue === "=") {
+      calculateResult()
+    } else {
+      appendValue(buttonValue)
+    }
   })
+}
+
+function clearResult() {
+
+}
+
+function calculateResult() {
+
+}
+
+function appendValue() {
+
 }
