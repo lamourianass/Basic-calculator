57866f072b98cc8adca7cd7629c1874abdca4d19
Anass Lamouri <anasslamouri@icloud.com>
2025-08-06 15:07:40 +0100

    Add an event listener to all buttons using a for loop

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const buttonsEl = document.querySelectorAll("button")
 
 
+for (let i = 0; i < buttonsEl.length; i++) {
+  buttonsEl[i].addEventListener("click", () => {
+    console.log(buttonsEl[i].textContent)
+  })
+}
